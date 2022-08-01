08ceb981461d5b4e3e0643348f5cbc9307baf1df
umar-mohammad <umar.mohammad@kcl.ac.uk>
2022-08-01 21:17:03 +0100

    make it better

--- server.js
+++ server.js
@@ -6,7 +6,12 @@ const data = require("./data.json")
 
 app.get("/cruises/:id", (req, res) => {
     const id = req.params.id
-    res.json(data.cruises.filter((cruise) => cruise.id == id))
+    const resultArray = data.cruises.filter((cruise) => cruise.id == id)
+    if (result.length > 0) {
+        res.json(resultArray[0])
+    } else {
+        res.status(404).send("Cruise Not Found")
+    }
 })
 
 app.get("/cruises", (req, res) => {
