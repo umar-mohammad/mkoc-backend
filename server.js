a2da201bd45fa2bed87166ff1727b18f17aa4292
umar-mohammad <umar.mohammad@kcl.ac.uk>
2022-08-01 20:45:41 +0100

    make it better

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const port = process.env.PORT || 3001
 
 const data = require("./data.json")
 
-app.get("/", (req, res) => {
+app.get("/cruises/:id", (req, res) => {
+    const { id } = req.params
+    res.json(data.cruises.filter((cruise) => cruise.id === id))
+})
+
+app.get("/cruises", (req, res) => {
     res.json(data)
 })
 
