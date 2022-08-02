0a3a36e23ca9661461da28bc6809db70694e63a2
umar-mohammad <umar.mohammad@kcl.ac.uk>
2022-08-02 12:34:04 +0100

    make it better

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.get("/cruises", (req, res) => {
     res.json(data)
 })
 
+app.get("/", (req, res) => {
+    res.redirect("/cruises")
+})
+
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
 })
